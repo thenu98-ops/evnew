7139300a486e1e6857b83c2b53f9c4304d6614ab
Thenura Karunarathna <154442881+thenu98-ops@users.noreply.github.com>
2025-03-12 13:35:58 +0530

    all debug done!

--- lib/pages/charging_screen.dart
+++ lib/pages/charging_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 import 'charging_history.dart';
 
 class ChargingScreen extends StatefulWidget {
